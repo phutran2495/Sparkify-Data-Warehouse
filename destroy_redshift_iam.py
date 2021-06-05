ddb701a9df758aa84b8e051e148b9a1d1e29b406
phu tran <tranphu2495@gmail.com>
2021-06-05 11:45:32 -0700

    finnish

---
+++ destroy_redshift_iam.py
@@ -0,0 +1,25 @@
+import boto3
+import configparser
+config = configparser.ConfigParser()
+config.read_file(open('dwh.cfg'))
+
+KEY                    = config.get('AWS','KEY')
+SECRET                 = config.get('AWS','SECRET')
+DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+
+redshift = boto3.client('redshift',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                       )
+iam = boto3.client('iam', region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET)
+
+# delete the cluster
+redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+
+#delete the Iam role
+iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
